3c240439b1078bc95606cecf6f36c044f2e431a6
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2026-06-16 12:37:15 +0330

    feat: Prototype paging alloc

--- src/main.rs
+++ src/main.rs
@@ -130,30 +130,29 @@ enum Node {
     Leaf(Leaf),
 }
 
+#[derive(Debug, Clone, Encode, Decode)]
+struct Free {
+    prev_free_page_id: u64,
+    next_free_page_id: u64,
+}
+
+#[derive(Debug, Clone, Encode, Decode)]
+enum PageContent {
+    Ready,
+    Free(Free),
+    Metadata(Metadata),
+    Node(Node),
+}
+
 const DAFTAR_SQL_MAGIC: u64 = 0xfedcba;
 
 #[derive(Debug, Clone, Encode, Decode)]
 struct Metadata {
     pub magic: u64,
     pub version: u64,
-    pub root_node: u64,
-}
-
-impl TryInto<Page> for Node {
-    type Error = anyhow::Error;
-    fn try_into(self) -> Result<Page, Self::Error> {
-        let bytes = bincode::encode_to_vec(self, bincode::config::standard())?;
-        bytes.try_into()
-    }
-}
-
-impl TryFrom<Page> for Node {
-    type Error = anyhow::Error;
-
-    fn try_from(page: Page) -> Result<Self, Self::Error> {
-        let (node, _) = bincode::decode_from_slice(&page.0, bincode::config::standard())?;
-        Ok(node)
-    }
+    pub root_node_page_id: u64,          // The page where root node exists
+    pub next_page_id: u64,               // Next unallocated page
+    pub first_free_page_id: Option<u64>, // First free page we can allocate
 }
 
 struct KvStore<S: Storage> {
@@ -161,57 +160,153 @@ struct KvStore<S: Storage> {
 }
 
 impl<S: Storage> KvStore<S> {
+    fn set_page_content(&mut self, idx: u64, content: PageContent) -> Result<(), anyhow::Error> {
+        let mut page = Page::default();
+        let ser = bincode::encode_to_vec(&content, bincode::config::standard())?;
+        page.0[..ser.len()].copy_from_slice(&ser);
+        self.storage.set_pages(vec![(idx, page)])?;
+        Ok(())
+    }
+
+    fn get_page_content(&self, idx: u64) -> Result<PageContent, anyhow::Error> {
+        let page = self
+            .storage
+            .get_page(idx)?
+            .ok_or(anyhow!("Page unavailable!"))?;
+        let (content, _): (PageContent, _) =
+            bincode::decode_from_slice(&page.0, bincode::config::standard())?;
+        Ok(content)
+    }
+
+    fn get_node_page(&self, page_id: u64) -> Result<Node, anyhow::Error> {
+        if let PageContent::Node(node) = self.get_page_content(page_id)? {
+            Ok(node)
+        } else {
+            Err(anyhow!("Expected a node!"))
+        }
+    }
+
+    fn get_free_page(&self, page_id: u64) -> Result<Free, anyhow::Error> {
+        if let PageContent::Free(node) = self.get_page_content(page_id)? {
+            Ok(node)
+        } else {
+            Err(anyhow!("Expected a free page!"))
+        }
+    }
+
+    fn free(&mut self, page_id: u64) -> Result<(), anyhow::Error> {
+        let mut metadata = self.metadata()?;
+        if let Some(first_free_page_id) = metadata.first_free_page_id {
+            let mut free = self.get_free_page(first_free_page_id)?;
+            self.set_page_content(
+                page_id,
+                PageContent::Free(Free {
+                    prev_free_page_id: first_free_page_id,
+                    next_free_page_id: free.next_free_page_id,
+                }),
+            )?;
+
+            let mut next_page = self.get_free_page(free.next_free_page_id)?;
+            next_page.prev_free_page_id = page_id;
+            self.set_page_content(free.next_free_page_id, PageContent::Free(next_page))?;
+
+            free.next_free_page_id = page_id;
+            if free.prev_free_page_id == first_free_page_id {
+                free.prev_free_page_id = page_id;
+            }
+            self.set_page_content(first_free_page_id, PageContent::Free(free))?;
+        } else {
+            self.set_page_content(
+                page_id,
+                PageContent::Free(Free {
+                    prev_free_page_id: page_id,
+                    next_free_page_id: page_id,
+                }),
+            )?;
+            metadata.first_free_page_id = Some(page_id);
+            self.set_page_content(0, PageContent::Metadata(metadata))?;
+        }
+        Ok(())
+    }
+
+    fn alloc_page(&mut self) -> Result<u64, anyhow::Error> {
+        let mut metadata = self.metadata()?;
+        if let Some(free_page_id) = metadata.first_free_page_id {
+            let free_page = self.get_free_page(free_page_id)?;
+
+            let mut prev_page = self.get_free_page(free_page.prev_free_page_id)?;
+            prev_page.next_free_page_id = free_page.next_free_page_id;
+            self.set_page_content(free_page.prev_free_page_id, PageContent::Free(prev_page))?;
+
+            let mut next_page = self.get_free_page(free_page.next_free_page_id)?;
+            next_page.prev_free_page_id = free_page.prev_free_page_id;
+            self.set_page_content(free_page.next_free_page_id, PageContent::Free(next_page))?;
+
+            self.set_page_content(free_page_id, PageContent::Ready)?;
+
+            metadata.first_free_page_id = if free_page.next_free_page_id != free_page_id {
+                Some(free_page.next_free_page_id)
+            } else {
+                None
+            };
+            self.set_page_content(0, PageContent::Metadata(metadata))?;
+            Ok(free_page_id)
+        } else {
+            let new_page_id = metadata.next_page_id;
+            self.set_page_content(new_page_id, PageContent::Ready)?;
+            metadata.next_page_id += 1;
+            self.set_page_content(0, PageContent::Metadata(metadata))?;
+            Ok(new_page_id)
+        }
+    }
+
     fn init(&mut self) -> Result<(), anyhow::Error> {
         if self.metadata().is_ok() {
             return Err(anyhow!("Already initialized!"));
         }
-        let mut page_0 = Page::default();
-        let metadata_ser = bincode::encode_to_vec(
-            &Metadata {
+        self.set_page_content(
+            0,
+            PageContent::Metadata(Metadata {
                 magic: DAFTAR_SQL_MAGIC,
                 version: 1,
-                root_node: 1,
-            },
-            bincode::config::standard(),
+                root_node_page_id: 1,
+                next_page_id: 2,
+                first_free_page_id: None,
+            }),
         )?;
-        page_0.0[..metadata_ser.len()].copy_from_slice(&metadata_ser);
-        self.storage.set_pages(vec![
-            (0, page_0),
-            (
-                1,
-                Node::Leaf(Leaf {
-                    keys: vec![],
-                    values: vec![],
-                    next: None,
-                })
-                .try_into()?,
-            ),
-        ])?;
+        self.set_page_content(
+            1,
+            PageContent::Node(Node::Leaf(Leaf {
+                keys: vec![],
+                values: vec![],
+                next: None,
+            })),
+        )?;
         Ok(())
     }
+
     fn metadata(&self) -> Result<Metadata, anyhow::Error> {
-        let page_0 = self
-            .storage
-            .get_page(0)?
-            .ok_or(anyhow!("Metadata unavailable!"))?;
-        let (metadata, _): (Metadata, _) =
-            bincode::decode_from_slice(&page_0.0, bincode::config::standard())?;
-        if metadata.magic != DAFTAR_SQL_MAGIC {
-            return Err(anyhow!("Invalid magic number!"));
+        match self.get_page_content(0)? {
+            PageContent::Metadata(metadata) => {
+                if metadata.magic != DAFTAR_SQL_MAGIC {
+                    return Err(anyhow!("Invalid magic number!"));
+                }
+                Ok(metadata)
+            }
+            _ => Err(anyhow!("Page 0 is not a metadata page!")),
         }
-        Ok(metadata)
     }
 
-    fn get_node(&self, node_id: u64) -> Result<Option<Node>, anyhow::Error> {
-        if let Some(page) = self.storage.get_page(node_id)? {
-            Ok(Some(page.try_into()?))
+    fn get_node(&self, page_id: u64) -> Result<Node, anyhow::Error> {
+        if let PageContent::Node(node) = self.get_page_content(page_id)? {
+            Ok(node)
         } else {
-            Ok(None)
+            Err(anyhow!("Node not found!"))
         }
     }
 
     fn find_leaf(&self, key: &Blob, node_id: u64) -> Result<(u64, Leaf), anyhow::Error> {
-        let node = self.get_node(node_id)?.ok_or(anyhow!("Node not found!"))?;
+        let node = self.get_node(node_id)?;
         Ok(match node {
             Node::Leaf(leaf) => (node_id, leaf),
             Node::Internal(internal) => {
@@ -226,7 +321,7 @@ impl<S: Storage> KvStore<S> {
     }
 
     fn insert(&mut self, pairs: Vec<(Blob, Blob)>) -> Result<(), anyhow::Error> {
-        let root_id = self.metadata()?.root_node;
+        let root_id = self.metadata()?.root_node_page_id;
         let mut fork = self.fork_mut();
         for (k, v) in pairs {
             let (node_id, mut leaf) = fork.find_leaf(&k, root_id)?;
@@ -239,15 +334,14 @@ impl<S: Storage> KvStore<S> {
                     leaf.values.insert(idx, v);
                 }
             }
-            fork.storage
-                .set_pages(vec![(node_id, Node::Leaf(leaf).try_into()?)])?;
+            fork.set_page_content(node_id, PageContent::Node(Node::Leaf(leaf)))?;
         }
         fork.storage.commit()?;
         Ok(())
     }
 
     fn get(&self, key: &Blob) -> Result<Option<Blob>, anyhow::Error> {
-        let root_id = self.metadata()?.root_node;
+        let root_id = self.metadata()?.root_node_page_id;
         let (_, leaf) = self.find_leaf(key, root_id)?;
         Ok(if let Ok(idx) = leaf.keys.binary_search(key) {
             Some(leaf.values[idx].clone())
@@ -284,8 +378,6 @@ fn main() -> Result<(), anyhow::Error> {
     ])?;
 
     println!("{:?}", db.get_node(1)?);
-
-    //println!("{:?}", db.get(&b"5".into())?);
 
     Ok(())
 }
